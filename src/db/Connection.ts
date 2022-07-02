684a46cbbd1c04b69b7e1fd9b82b9919f3bf0fba
Kamil Jakubus <kamiljkbz@protonmail.com>
2022-07-02 04:38:18 +0200

    feat: add initial realtime support for DB

--- src/db/Connection.ts
+++ src/db/Connection.ts
@@ -1,17 +1,42 @@
-import type { DatabasePool, DatabasePoolConnection } from "slonik";
+import type { DatabasePool } from "slonik";
 import { createPool } from "slonik";
+
+import createSubscriber, { Subscriber } from "pg-listen";
 
 import type { Buidl3QueryMethods } from "./Query";
 import * as QueryMethods from "./Query";
 
-type Buidl3Pool = DatabasePool & Buidl3QueryMethods;
-
-let pool: DatabasePool | null = null;
+type Buidl3Subscriber = Subscriber<{ [channel: string]: any }>;
+type Buidl3Pool = DatabasePool &
+  Buidl3QueryMethods & { realtime: Buidl3Subscriber };
 
 export async function create(): Promise<Buidl3Pool> {
-  if (!pool) {
-    pool = await createPool(process.env.DB_CONNECT as string);
+  const pool = await createPool(process.env.DB_CONNECT as string);
+  const subscriber = createSubscriber({
+    connectionString: process.env.DB_CONNECT as string,
+  });
+
+  const notifications = subscriber.notifications;
+  const subscribe = notifications.on;
+
+  function subscribeWithListen(
+    ...parameters: Parameters<typeof notifications.on>
+  ): ReturnType<typeof notifications.on> {
+    const channel = parameters[0] as string;
+
+    const result = subscribe.apply(notifications, parameters);
+    subscriber.listenTo(channel);
+
+    return result;
   }
 
-  return { ...pool, ...(QueryMethods as Buidl3QueryMethods) };
+  subscriber.notifications.on = subscribeWithListen;
+
+  await subscriber.connect();
+
+  return {
+    ...pool,
+    realtime: subscriber,
+    ...(QueryMethods as Buidl3QueryMethods),
+  };
 }
